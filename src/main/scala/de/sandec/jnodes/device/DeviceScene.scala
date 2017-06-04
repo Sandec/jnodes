7d150f5620b38e090bfd3d1f61817dfd49d249d0
floriankirmaier <florian.kirmaier@gmail.com>
2017-06-04 22:15:30 +0200

    small api-improvement

--- src/main/scala/de/sandec/jnodes/device/DeviceScene.scala
+++ src/main/scala/de/sandec/jnodes/device/DeviceScene.scala
@@ -4,8 +4,8 @@ import de.sandec.jnodes.elements.Resizable
 import simplefx.all._
 import simplefx.core._
 
-class DeviceScene(_root: Resizable with Parent, _width: Double, _height: Double)
-  extends Scene(_root, _width: Double, _height: Double) { THIS =>
+class DeviceScene(_root: Node, _width: Double, _height: Double)
+  extends Scene(new Group, _width: Double, _height: Double) { THIS =>
 
   @Bind var device = <*> (deviceNode.device)
   @Bind val deviceNode = new Device(_root)
