10b1498e0052cf9cac961b9a91f349f475ab8bd7
floriankirmaier <florian.kirmaier@gmail.com>
2017-06-04 18:17:45 +0200

    added swipes to the Carousel

--- src/main/scala/de/sandec/jnodes/elements/Carousel.scala
+++ src/main/scala/de/sandec/jnodes/elements/Carousel.scala
@@ -27,10 +27,8 @@ class Carousel extends StackPane { THIS =>
     currentPlace := currentTarget in (1 s) using Interpolator.EASE_BOTH
   }
 
-  @Bind val rightButton = new Region{r => javafx.scene.layout.StackPane.setAlignment(r, Pos.CENTER_RIGHT); styleClass ::= STYLE_NAME + "-right"; this.onClick --> onLeft }
-  @Bind val leftButton  = new Region{r => javafx.scene.layout.StackPane.setAlignment(r, Pos.CENTER_LEFT ); styleClass ::= STYLE_NAME + "-left" ; this.onClick --> onRight}
-
-
+  @Bind val rightButton = new Region{r => javafx.scene.layout.StackPane.setAlignment(r, Pos.CENTER_RIGHT); styleClass ::= STYLE_NAME + "-right"}
+  @Bind val leftButton  = new Region{r => javafx.scene.layout.StackPane.setAlignment(r, Pos.CENTER_LEFT ); styleClass ::= STYLE_NAME + "-left" }
 
   def manageElem(node: Node, place: Int) = {
     node.visible <-- (((place-1) < currentPlace) && (place+1 > currentPlace))
@@ -53,4 +51,39 @@ class Carousel extends StackPane { THIS =>
 
   this <++ rightButton
   this <++ leftButton
+
+
+  rightButton.onMouseClicked --> { e => if(e.isStillSincePress) onLeft  }
+  leftButton .onMouseClicked --> { e => if(e.isStillSincePress) onRight }
+
+  this.onMousePressed --> { e =>
+    val start = (e.getX, e.getY)
+    val startT = systemTime
+    var disp: Disposer = null
+    disp = onMouseReleased --> { (e2: MouseEvent) =>
+      val end = (e2.getX, e2.getY)
+      val endT = systemTime
+
+      def change = (end - start)
+
+      def cond1 = !e2.isStillSincePress
+      def cond2 = (end - start).length > 20
+
+      def condLeft  = change._1 < 20
+      def condRight = change._1 > 20
+
+      def condHorizontal = change._1.abs > change._2.abs
+
+      def isLeft = cond1 && cond2 && condLeft && condHorizontal
+      def isRight = cond1 && cond2 && condRight && condHorizontal
+
+      if(isLeft)  onLeft
+      if(isRight) onRight
+      disp.dispose
+    }
+
+  }
+
+  this.onSwipeLeft  --> { onLeft  }
+  this.onSwipeRight --> { onRight }
 }
