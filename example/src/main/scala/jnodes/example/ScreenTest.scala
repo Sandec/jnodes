25d6e4e15b641294add03ba51f47126ce4bea2e3
floriankirmaier <florian.kirmaier@gmail.com>
2017-10-19 18:15:22 +0200

    new jnodes-release

--- example/src/main/scala/jnodes/example/ScreenTest.scala
+++ example/src/main/scala/jnodes/example/ScreenTest.scala
@@ -1,6 +1,7 @@
 package jnodes.example
 
 import de.sandec.jnodes.device.DemoContainer
+import de.sandec.jnodes.device.{DemoContainer}
 import simplefx.all._
 import simplefx.core._
 
@@ -9,8 +10,8 @@ object ScreenTest extends App
 class ScreenTest { PARENT =>
     title = "Some demo"
 
-    scene = new Scene(new DemoContainer(new StackPane, new StackPane, new StackPane, new StackPane, new StackPane){
+    scene = new Scene(new StackPane(new DemoContainer(new StackPane, new StackPane, new StackPane, new StackPane, new StackPane){
       @Bind override var imageLocation = "/test/image/flowers.jpg"
-    }, 1200, 600)
+    }){padding = Insets(20)}, 1200, 600)
 
 }
