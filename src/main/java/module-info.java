4510b0f740563a4bc0104850ad493634f21c173f
Besmir Beqiri <besmir.beqiri@logicbind.io>
2023-05-05 15:43:14 +0200

    Export the missing packages in `de.sandec.jnodes` module descriptor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,8 @@
 module de.sandec.jnodes {
     requires transitive javafx.controls;
 
+    exports de.sandec.jnodes.context;
     exports de.sandec.jnodes.fork;
+    exports de.sandec.jnodes.css;
+    exports de.sandec.jnodes.elements;
 }
